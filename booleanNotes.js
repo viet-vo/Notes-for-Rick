6c0f21c7f07b64d1582b2c81b8c63a9ee62e37e1
Viet Vo <viet.allen.qvo@gmail.com>
2021-04-25 17:09:04 -0700

    Adding boolean notes

---
+++ booleanNotes.js
@@ -0,0 +1,26 @@
+// boolean
+var one = 1;
+var yes = true;
+var zero = 0;
+var no = "false";
+
+if (yes == true) {
+  console.log("1yes");
+}
+// yes
+if (yes) {
+  console.log("2yes");
+}
+// yes
+if (no == true) {
+  console.log("no");
+}
+// no
+if (zero == true) {
+  console.log("no");
+}
+// no
+if (one == true) {
+  console.log("3yes equals true");
+}
+// yes
